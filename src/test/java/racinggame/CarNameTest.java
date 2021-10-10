23a85e66d0e76c8702e34fb103359cc52ddf36ec
JungHoon <junghoon.sin@nts-corp.com>
2021-10-10 13:50:07 +0900

    <refactor> 테스트 이름 변경

--- src/test/java/racinggame/CarNameTest.java
+++ src/test/java/racinggame/CarNameTest.java
@@ -13,14 +13,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CarNameTest {
 
     @Test
-    void 자동차_이름_입력_받기() {
+    void 이름_입력_받기() {
         String input = "pobi,crong,honux";
         CarName carName = new CarName(input);
         assertEquals(input, carName.getNames());
     }
 
     @Test
-    void 자동차_빈문자열_입력시_에러() {
+    void 빈문자열_입력시_에러() {
         assertThrows(IllegalArgumentException.class, () -> new CarName(""));
     }
 }
