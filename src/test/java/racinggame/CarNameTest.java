208ffbf68cfacf46967a3e3f8ecdc260734df19c
JungHoon <junghoon.sin@nts-corp.com>
2021-10-11 22:18:22 +0900

    <refactor> 미사용 import 정리

--- src/test/java/racinggame/CarNameTest.java
+++ src/test/java/racinggame/CarNameTest.java
@@ -3,7 +3,6 @@ package racinggame;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CarNameTest {
